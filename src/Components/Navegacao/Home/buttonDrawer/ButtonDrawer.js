a27b08f09f74c9f82f80a815cc74ad785ab934ce
GaLfoTorTo <leonardovl117@gmail.com>
2021-02-04 10:51:16 -0300

    funcionalidade do toggle do drawer

--- src/Components/Navegacao/Home/buttonDrawer/ButtonDrawer.js
+++ src/Components/Navegacao/Home/buttonDrawer/ButtonDrawer.js
@@ -1,15 +1,16 @@
+import { DrawerActions } from '@react-navigation/native';
 import React from 'react';
 import {TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import estilo from './estilo';
 
-const ButtonDrawer = ({navigation}) => {
+const ButtonDrawer = ({ navigation }) => {
     return (
         <TouchableOpacity
             style={estilo.botao}
-            onPress={() => navigation.openDrawer()}
+            onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
         >
-            <Icon name='menu' size={30} style={estilo.icone}/>
+            <Icon name='send' size={30} style={estilo.icone}/>
         </TouchableOpacity>
     )
 }
